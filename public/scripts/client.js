97b004236a115c7dd67dffc3e201b59a5eaa4c0d
Hosu <sogeumhosu@gmail.com>
2023-08-16 13:55:48 -0700

    Implemented functions to render tweets dynamically

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,23 +1,68 @@
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  user: {
-    name: 'Newton',
-    avatars: 'https://i.imgur.com/73hZDYK.png',
-    handle: '@SirIsaac',
-  },
-  content: {
-    text: 'If I have seen further it is by standing on the shoulders of giants',
-  },
-  created_at: 1461116232227,
-};
+$(document).ready(() => {
+  const data = [
+    {
+      user: {
+        name: 'Newton',
+        avatars: 'https://i.imgur.com/73hZDYK.png',
+        handle: '@SirIsaac',
+      },
+      content: {
+        text: 'If I have seen further it is by standing on the shoulders of giants',
+      },
+      created_at: 1461116232227,
+    },
+    {
+      user: {
+        name: 'Descartes',
+        avatars: 'https://i.imgur.com/nlhLi3I.png',
+        handle: '@rd',
+      },
+      content: {
+        text: 'Je pense , donc je suis',
+      },
+      created_at: 1461113959088,
+    },
+  ];
 
-function createTweetElement(tweetData) {
-  const $tweet = `<article class="tweet">Hello world</article>`;
-  return $tweet;
-}
+  const renderTweets = function (tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    for (const tweet of tweets) {
+      let insertTweet = createTweetElement(tweet);
+      $('#tweets-container').append(insertTweet);
+    }
+  };
 
-const $tweet = createTweetElement(tweetData);
+  const createTweetElement = function (tweet) {
+    let $tweet =
+      /* Your code for creating the tweet element */
+      `
+<article class="tweet">
+<header class="tweet-header">
+  <div class="user-name">
+    <img src="${tweet.user.avatars}" />
+    <span class="name">${tweet.user.name}</span>
+  </div>
+  <span class="user-id">${tweet.user.handle}</span>
+</header>
+<p class="post-content">
+  ${tweet.content.text}
+</p>
+<div class="divider"></div>
+<footer class="tweet-footer">
+  <span class="post-date">${tweet.created_at}</span>
+  <div class="icons">
+    <i class="fa fa-flag"></i>
+    <i class="fa fa-retweet"></i>
+    <i class="fa fa-heart"></i>
+  </div>
+</footer>
+</article>
+`;
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+    return $tweet;
+  };
+
+  renderTweets(data);
+});
