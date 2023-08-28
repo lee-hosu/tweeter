7dd73bf48d18ddef30fef2c80048e8ebe951ef0c
Hosu <sogeumhosu@gmail.com>
2023-08-28 10:57:55 -0700

    Implement loadTweets function for tweet loading from the server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,33 +1,5 @@
 $(document).ready(() => {
-  const data = [
-    {
-      user: {
-        name: 'Newton',
-        avatars: 'https://i.imgur.com/73hZDYK.png',
-        handle: '@SirIsaac',
-      },
-      content: {
-        text: 'If I have seen further it is by standing on the shoulders of giants',
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: 'Descartes',
-        avatars: 'https://i.imgur.com/nlhLi3I.png',
-        handle: '@rd',
-      },
-      content: {
-        text: 'Je pense , donc je suis',
-      },
-      created_at: 1461113959088,
-    },
-  ];
-
   const renderTweets = function (tweets) {
-    // loops through tweets
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
     for (const tweet of tweets) {
       let insertTweet = createTweetElement(tweet);
       $('#tweets-container').append(insertTweet);
@@ -61,7 +33,21 @@ $(document).ready(() => {
     return $tweet;
   };
 
-  renderTweets(data);
+  const loadTweets = function () {
+    $.ajax({
+      type: 'GET',
+      url: '/tweets',
+      dataType: 'json',
+      success: function (tweets) {
+        renderTweets(tweets);
+      },
+      error: function (error) {
+        console.log('Error:', error);
+      },
+    });
+  };
+
+  loadTweets();
 
   // Listen to Form submit event
   const $newTweetForm = $('form');
@@ -74,6 +60,7 @@ $(document).ready(() => {
       data: message,
       success: function (response) {
         console.log('Success:', response);
+        loadTweets();
       },
       error: function (error) {
         console.log('Error:', error);
