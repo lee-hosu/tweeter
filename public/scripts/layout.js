8b7595cd0baed53cd26931bbdfb5a822b2178c83
Hosu <sogeumhosu@gmail.com>
2023-09-02 20:21:11 -0700

    Fix mobile breakpoint to 1024

--- public/scripts/layout.js
+++ public/scripts/layout.js
@@ -1,8 +1,8 @@
 $(document).ready(() => {
   $(window).on('scroll resize', function () {
-    if ($(this).width() < 980 && $(this).scrollTop() > 200) {
+    if ($(this).width() < 1024 && $(this).scrollTop() > 200) {
       $('nav').css('background-color', '#4056a1');
-    } else if ($(this).width() > 980) {
+    } else if ($(this).width() > 1024) {
       $('nav').css('background-color', '#4056a1');
     } else {
       $('nav').css('background-color', 'transparent');
